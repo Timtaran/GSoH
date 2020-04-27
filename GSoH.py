b6150a30d71c9084795d17f9b235f0b618b17209
Timtaran <60805088+Timtaran@users.noreply.github.com>
2020-04-27 22:43:15 +0500

    Add files via upload

---
+++ GSoH.py
@@ -0,0 +1,557 @@
+import hashlib
+import random as ra
+r = ra.randint(1, 25)
+s=ra.randint(1, 25)
+t=ra.randint(1, 25)
+u=ra.randint(1, 25)
+if r == 1:
+    r='a'
+if r == 2:
+    r='b'
+if r == 3:
+    r='c'
+if r == 4:
+    r='e'
+if r == 5:
+    r='f'
+if r == 6:
+    r='g'
+if r == 7:
+    r='h'
+if r == 8:
+    r='i'
+if r == 9:
+    r='j'
+if r == 10:
+    r='k'
+if r == 11:
+    r='l'
+if r == 12:
+    r='m'
+if r == 13:
+    r='n'
+if r == 14:
+    r='o'
+if r == 15:
+    r='p'
+if r == 16:
+    r='q'
+if r == 17:
+    r='r'
+if r == 18:
+    r='s'
+if r == 19:
+    r='t'
+if r == 20:
+    r='u'
+if r == 21:
+    r='v'
+if r == 22:
+    r='w'
+if r == 23:
+    r='x'
+if r == 24:
+    r='y'
+if r == 25:
+    r='z'
+if s == 1:
+    s='a'
+if s == 2:
+    s='b'
+if s == 3:
+    s='c'
+if s == 4:
+    s='d'
+if s == 5:
+    s='e'
+if s == 6:
+    s='f'
+if s == 7:
+    s='g'
+if s == 8:
+    s='h'
+if s == 9:
+    s='i'
+if s == 10:
+    s='j'
+if s == 11:
+    s='k'
+if s == 12:
+    s='l'
+if s == 13:
+    s='m'
+if s == 14:
+    s='n'
+if s == 15:
+    s='o'
+if s == 16:
+    s='p'
+if s == 17:
+    s='q'
+if s == 18:
+    s='r'
+if s == 19:
+    s='s'
+if s == 20:
+    s='t'
+if s == 21:
+    s='u'
+if s == 22:
+    s='v'
+if s == 23:
+    s='x'
+if s == 24:
+    s='y'
+if s == 25:
+    s='z'
+if t == 1:
+    t='a'
+if t == 2:
+    t='b'
+if t == 3:
+    t='c'
+if t == 4:
+    t='e'
+if t == 5:
+    t='f'
+if t == 6:
+    t='g'
+if t == 7:
+    t='h'
+if t == 8:
+    t='i'
+if t == 9:
+    t='j'
+if t == 10:
+    t='k'
+if t == 11:
+    t='l'
+if t == 12:
+    t='m'
+if t == 13:
+    t='n'
+if t == 14:
+    t='o'
+if t == 15:
+    t='p'
+if t == 16:
+    t='q'
+if t == 17:
+    t='r'
+if t == 18:
+    t='s'
+if t == 19:
+    t='t'
+if t == 20:
+    t='u'
+if t == 21:
+    t='v'
+if t == 22:
+    t='w'
+if t == 23:
+    t='x'
+if t == 24:
+    t='y'
+if t == 25:
+    t='z'
+if u == 1:
+    u='a'
+if u == 2:
+    u='b'
+if u == 3:
+    u='c'
+if u == 4:
+    u='e'
+if u == 5:
+    u='f'
+if u == 6:
+    u='g'
+if u == 7:
+    u='h'
+if u == 8:
+    u='i'
+if u == 9:
+    u='j'
+if u == 10:
+    u='k'
+if u == 11:
+    u='l'
+if u == 12:
+    u='m'
+if u == 13:
+    u='n'
+if u == 14:
+    u='o'
+if u == 15:
+    u='p'
+if u == 16:
+    u='q'
+if u == 17:
+    u='r'
+if u == 18:
+    u='s'
+if u == 19:
+    u='t'
+if u == 20:
+    u='u'
+if u == 21:
+    u='v'
+if u == 22:
+    u='w'
+if u == 23:
+    u='x'
+if u == 24:
+    u='y'
+if u == 25:
+    u='z'
+if u == 1:
+    u='a'
+if u == 2:
+    u='b'
+if u == 3:
+    u='c'
+if u == 4:
+    u='e'
+if u == 5:
+    u='f'
+if u == 6:
+    u='g'
+if u == 7:
+    u='h'
+if u == 8:
+    u='i'
+if u == 9:
+    u='j'
+if u == 10:
+    u='k'
+if u == 11:
+    u='l'
+if u == 12:
+    u='m'
+if u == 13:
+    u='n'
+if u == 14:
+    u='o'
+if u == 15:
+    u='p'
+if u == 16:
+    u='q'
+if u == 17:
+    u='r'
+if u == 18:
+    u='s'
+if u == 19:
+    u='t'
+if u == 20:
+    u='u'
+if u == 21:
+    u='v'
+if u == 22:
+    u='w'
+if u == 23:
+    u='x'
+if u == 24:
+    u='y'
+if u == 25:
+    u='z'
+v=ra.randint(1, 25)
+if v == 1:
+    v='a'
+if v == 2:
+    v='b'
+if v == 3:
+    v='c'
+if v == 4:
+    v='e'
+if v == 5:
+    v='f'
+if v == 6:
+    v='g'
+if v == 7:
+    v='h'
+if v == 8:
+    v='i'
+if v == 9:
+    v='j'
+if v == 10:
+    v='k'
+if v == 11:
+    v='l'
+if v == 12:
+    v='m'
+if v == 13:
+    v='n'
+if v == 14:
+    v='o'
+if v == 15:
+    v='p'
+if v == 16:
+    v='q'
+if v == 17:
+    v='r'
+if v == 18:
+    v='s'
+if v == 19:
+    v='t'
+if v == 20:
+    v='u'
+if v == 21:
+    v='v'
+if v == 22:
+    v='w'
+if v == 23:
+    v='x'
+if v == 24:
+    v='y'
+if v == 25:
+    v='z'
+w=ra.randint(1, 25)
+if w == 1:
+    w='a'
+if w == 2:
+    w='b'
+if w == 3:
+    w='c'
+if w == 4:
+    w='e'
+if w == 5:
+    w='f'
+if w == 6:
+    w='g'
+if w == 7:
+    w='h'
+if w == 8:
+    w='i'
+if w == 9:
+    w='j'
+if w == 10:
+    w='k'
+if w == 11:
+    w='l'
+if w == 12:
+    w='m'
+if w == 13:
+    w='n'
+if w == 14:
+    w='o'
+if w == 15:
+    w='p'
+if w == 16:
+    w='q'
+if w == 17:
+    w='r'
+if w == 18:
+    w='s'
+if w == 19:
+    w='t'
+if w == 20:
+    w='u'
+if w == 21:
+    w='v'
+if w == 22:
+    w='w'
+if w == 23:
+    w='x'
+if w == 24:
+    w='y'
+if w == 25:
+    w='z'
+x=ra.randint(1, 25)
+if x == 1:
+    x='a'
+if x == 2:
+    x='b'
+if x == 3:
+    x='c'
+if x == 4:
+    x='e'
+if x == 5:
+    x='f'
+if x == 6:
+    x='g'
+if x == 7:
+    x='h'
+if x == 8:
+    x='i'
+if x == 9:
+    x='j'
+if x == 10:
+    x='k'
+if x == 11:
+    x='l'
+if x == 12:
+    x='m'
+if x == 13:
+    x='n'
+if x == 14:
+    x='o'
+if x == 15:
+    x='p'
+if x == 16:
+    x='q'
+if x == 17:
+    x='r'
+if x == 18:
+    x='s'
+if x == 19:
+    x='t'
+if x == 20:
+    x='u'
+if x == 21:
+    x='v'
+if x == 22:
+    x='w'
+if x == 23:
+    x='x'
+if x == 24:
+    x='y'
+if x == 25:
+    x='z'
+y=ra.randint(1, 25)
+if y == 1:
+    y='a'
+if y == 2:
+    y='b'
+if y == 3:
+    y='c'
+if y == 4:
+    y='e'
+if y == 5:
+    y='f'
+if y == 6:
+    y='g'
+if y == 7:
+    y='h'
+if y == 8:
+    y='i'
+if y == 9:
+    y='j'
+if y == 10:
+    y='k'
+if y == 11:
+    y='l'
+if y == 12:
+    y='m'
+if y == 13:
+    y='n'
+if y == 14:
+    y='o'
+if y == 15:
+    y='p'
+if y == 16:
+    y='q'
+if y == 17:
+    y='r'
+if y == 18:
+    y='s'
+if y == 19:
+    y='t'
+if y == 20:
+    y='u'
+if y == 21:
+    y='v'
+if y == 22:
+    y='w'
+if y == 23:
+    y='x'
+if y == 24:
+    y='y'
+if y == 25:
+    y='z'
+z=ra.randint(1, 25)
+if z == 1:
+    z='a'
+if z == 2:
+    z='b'
+if z == 3:
+    z='c'
+if z == 4:
+    z='e'
+if z == 5:
+    z='f'
+if z == 6:
+    z='g'
+if z == 7:
+    z='h'
+if z == 8:
+    z='i'
+if z == 9:
+    z='j'
+if z == 10:
+    z='k'
+if z == 11:
+    z='l'
+if z == 12:
+    z='m'
+if z == 13:
+    z='n'
+if z == 14:
+    z='o'
+if z == 15:
+    z='p'
+if z == 16:
+    z='q'
+if z == 17:
+    z='r'
+if z == 18:
+    z='s'
+if z == 19:
+    z='t'
+if z == 20:
+    z='u'
+if z == 21:
+    z='v'
+if z == 22:
+    z='w'
+if z == 23:
+    z='x'
+if z == 24:
+    z='y'
+if z == 25:
+    z='z'
+g=ra.randint(1,2)
+if g==1:
+    f=ra.randint(1, 7)
+    if f == 1:
+        f='!'
+    if f == 2:
+        f='@'
+    if f == 3:
+            f='#'
+    if f == 4:
+        f='$'
+    if f == 5:
+        f='%'
+    if f == 6:
+        f='&'
+    if f == 7:
+        f='*'
+else:
+    f = ''
+yt = 'No' #your text?
+if yt == 'No'
+    hc = '|{f}{r}{s}{t}{u}{v}{w}{x}{y}{z}'.format(f=f, r=r, s=s, t=t, u=u, v=v, w=w, x=x, y=y, z=z)
+else:
+    hc = '' #your text
+yn = '64' #your number or text(don't use whitespace please)
+hc = yn + hc
+hashvalue=hashlib.sha256(hc.encode())
+a = '\n\n'+hashvalue.hexdigest()
+b = hashvalue.hexdigest()
+print(a)
+send_message = 'Yes' #Yes or Not
+token = '' #token of vk.com (if 544 strings answer is "Yes")
+ID =  #your ID on vk.com
+if send_message == 'Yes':
+    import VKStarti as bot
+    import vk_api
+    vk = vk_api.VkApi(token=token)
+    vk = vk.get_api()
+
+    try:
+        c = vk.messages.search(q='String:', peer_id=ID, count=1)
+        c1 = c['items'][0]['conversation_message_id']
+        vk.messages.delete(message_ids=c1,delete_for_all=1)
+    except:
+       print('error on delete message')
+    bot.sms(549204433, 'String: ' + hc + '\nHash: ' + b)
